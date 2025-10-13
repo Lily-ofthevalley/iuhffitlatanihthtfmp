84e84b292b624f9780ff8bb69854be1298d2f273
Maarten <50550545+mmvanheusden@users.noreply.github.com>
2025-10-13 14:13:12 +0200

    Main menu (#1)
    * feat: main menu with functional start button
    
    * feat: functional main menu
    
    * fix: remove useless bus layout
    
    * fix: set main scene to main menu
    
    * refactor: cleanup

---
+++ scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	pass # Replace with function body.
+
+
+func _on_info_pressed() -> void:
+	$Popup.show()
+	pass # Replace with function body.
+
+
+func _on_popup_close() -> void:
+	$Popup.hide()
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.

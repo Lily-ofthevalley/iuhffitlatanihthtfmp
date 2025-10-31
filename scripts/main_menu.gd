a9b61910f9532498ddd01cca724c274d75890280
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2025-10-31 16:59:29 +0100

    feat: credits,settings buttons for main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -15,3 +15,11 @@ func _on_popup_close() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	pass
+
+
+func _on_credits_pressed() -> void:
+	pass

87569e3a44b44ca7cffd40014e25a2ead28f711d
ScolipedeFucker <maynouari@gmail.com>
2025-11-02 23:29:35 +0100

    Revert "feat: scene change ti_lab -> hallway (#14)"
    This reverts commit b1a64ef4fd4b6c7c35c1fbd787f30f793cbf8739.

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,18 +1,1 @@
 extends Node
-
-var current_scene = "ti_lab"
-
-var transition_scene = false
-
-var player_exit_closet_posz = 0
-var player_exit_closet_posx = 0
-var player_start_closet_posz = 0
-var player_start_closet_posx = 0
-
-func finish_changescenes():
-	if transition_scene == true:
-		transition_scene = false
-	if current_scene == "ti_lab":
-		current_scene = "home"
-	else :
-		current_scene = "ti_lab"

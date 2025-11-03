2e744e81cdc8aa3b57768a51e99a6cd4a4a1d248
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2025-11-03 11:04:39 +0100

    refactor: replace background with moving image of ti lab

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+
+func _ready():
+	$Backgroud/Moving.play("move-loop")
+
 func _on_start_pressed() -> void:
 	var loading_screen = load("res://scenes/loading_screen.tscn")
 	get_tree().change_scene_to_packed(loading_screen)

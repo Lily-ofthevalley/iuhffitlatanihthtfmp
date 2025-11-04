ebe53999734994e914c5474ecda53173a5905232
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2025-11-04 22:27:09 +0100

    chore: push progress

--- scripts/lock_pick_minigame.gd
+++ scripts/lock_pick_minigame.gd
@@ -1,16 +1,17 @@
 extends Control
 
 @onready var arduino = $Arduino
-@onready var circle = $Panel/Line2D
+@onready var circle = $Panel/Circle
 
 var old_value = 0
 
 func _ready():
-	arduino.calibrate_knob()
+	_on_calibrate_button_pressed()
 	set_process(true)
 
 func _process(delta: float) -> void:
 	var new_value = arduino.get_knob_relative_value()
+	#print(new_value)
 	if new_value != old_value:
 		var diff = new_value - old_value
 		print(diff)
@@ -19,3 +20,11 @@ func _process(delta: float) -> void:
 		for i in range(abs(diff)):
 			circle.shift_knob(direction)
 	old_value = new_value
+
+
+func _on_calibrate_button_pressed() -> void:
+	set_process(false)
+	arduino.calibrate_knob()
+	circle.reset()
+	old_value = 0
+	set_process(true)
--- scripts/rotary_circle.gd
+++ scripts/rotary_circle.gd
@@ -1,33 +1,32 @@
 @tool
 extends Polygon2D
 
-class_name Circle2D
+class_name Circle
 # https://forum.godotengine.org/t/how-to-create-a-circle-with-line2d/37813
 enum Direction{CLOCKWISE=0, COUNTERCLOCKWISE=1}
 
-@export var knob_indicator: TextureRect
+@export var knob_indicator: Line2D
 @export var knob_target: TextureRect
-
-@export var cuts = 128:
+@export var points = 15: # The amount of points the polygon circle should have.
 	get:
-		return cuts
+		return points
 	set(value):
-		cuts = value
-		
+		points = value
+
 		update_points()
 
-@export var STEP = cuts / 15
-var knob_value = 0
+@export var STEP = points / 15 # 1 step = 2 steps on the rotary encoder
+var knob_value = points + 3 # Should be the same as the relative value of the rotary encoder.
 
 func update_points():
 	var pos_list: PackedVector2Array = PackedVector2Array()
-	var correct_cuts = cuts + 3
-	
+	var correct_cuts = points + 3
+
 	for cut in range(correct_cuts):
 		var radian = (2 * PI / 360) * (360 / float(correct_cuts)) * (cut + 3) - PI/2
 		var radian_pos = Vector2(snapped(cos(radian), 0.01), snapped(sin(radian), 0.01))
 		pos_list.append(radian_pos)
-	
+
 	polygon = pos_list
 
 
@@ -35,23 +34,35 @@ func shift_knob(direction: Direction):
 	match direction:
 		Direction.CLOCKWISE:
 			knob_value += 1
-			knob_indicator.position = polygon[knob_value % cuts + STEP] * scale + position
+			knob_indicator.position = polygon[knob_value % points + STEP] * scale + position
+			_rotate_knob_indicator()
 		Direction.COUNTERCLOCKWISE:
 			knob_value -= 1
-			knob_indicator.position = polygon[knob_value % cuts + STEP] * scale + position
-
-func _set_knob_indicator(new_position: Vector2):
-	knob_indicator.position = new_position
+			knob_indicator.position = polygon[knob_value % points + STEP] * scale + position
+			_rotate_knob_indicator()
+	print(knob_value)
 	
 
+
+func _set_knob_indicator_relative_position(new_position: Vector2):
+	knob_indicator.position = new_position * scale + position
+
+func _rotate_knob_indicator():
+		# Rotate the indicator so it is perpendicular to the circle
+		var angle = (2 * PI / 360) * (360 / float(points + 3)) * (knob_value % points + STEP + 3) - PI/2 # Magic
+		knob_indicator.rotation = angle
+	
+func reset():
+	knob_value = -6
+	_set_knob_indicator_relative_position(polygon[points/4 - 6])
+	_rotate_knob_indicator()
 
 func _ready() -> void:
-	# Get random point, and instance the marker
+	# Get random point, and put the marker there
 	var random_pos = polygon[randi() % polygon.size()]
 	knob_target.position = random_pos * scale + position
-	knob_indicator.position = polygon[0]
-	
-	#
+	# TODO: calibrate Arduino as well.
+	reset()
 	#while true:
 		#for i in range(40*200):
 			#print(i % 40)

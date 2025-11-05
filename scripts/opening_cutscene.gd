73d24829fb785b292cb765c7483f75afe66b5122
Lily-ofthevalley <lilithvdplas@gmail.com>
2025-11-05 15:12:18 +0100

    added text animations

--- scripts/opening_cutscene.gd
+++ scripts/opening_cutscene.gd
@@ -6,6 +6,10 @@ var dialogue = []
 var current_dialogue_id = 0
 var d_active = false
 
+var letters_per_second: float = 20
+var dialogue_size: int = 0
+var tween: Tween
+
 func _ready() -> void:
 	if !d_active:
 		$MarginContainer.visible = false
@@ -13,16 +17,32 @@ func _ready() -> void:
 	set_process(true)
 
 func play(timer: SceneTreeTimer):
+	if tween != null:
+		tween.kill()
+		
 	d_active = true
 	$MarginContainer.visible = true
 	dialogue = load_dialogue()
 	print(dialogue)
 	
 	set_dialogue(dialogue[0])
+	
+	$MarginContainer/Panel/Text.visible_characters = 0
+	dialogue_size = $MarginContainer/Panel/Text.get_total_character_count()
+	
+	tween = create_tween()
+	tween.tween_property($MarginContainer/Panel/Text, "visible_characters", dialogue_size, dialogue_size / letters_per_second)
+	
 	await timer.timeout
 	
 	set_dialogue(dialogue[1])
+	
+	await tween.finished
+	await get_tree().create_timer(1.0).timeout
+	
 	opening_cutscene_finish.emit()
+	
+	
 
 func close():
 	$MarginContainer.visible = false

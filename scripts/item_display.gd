e1dcbb5f1075c39707710f9f7b75d8b8c7145e6f
ScolipedeFucker <maynouari@gmail.com>
2025-11-06 12:10:05 +0100

    Inventory (#30)
    * Merged from 'main' into Inventory
    
    * Merged stashed changes
    
    * Revert "feat: scene change ti_lab -> hallway (#14)"
    
    This reverts commit b1a64ef4fd4b6c7c35c1fbd787f30f793cbf8739.
    
    * ok
    
    * fixed issue where item pickup would return fatal error
    
    * removed redundant chests
    
    * Fixed item_display.gd from broken merge
    
    * Created new sprites for- and refactored style of Inventory

---
+++ scripts/item_display.gd
@@ -0,0 +1,9 @@
+extends Panel
+
+@onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
+func update(slot: InvSlot):
+		if slot.item:
+			item_visual.visible = true
+			item_visual.texture = slot.item.texture
+		else:
+			item_visual.visible = false

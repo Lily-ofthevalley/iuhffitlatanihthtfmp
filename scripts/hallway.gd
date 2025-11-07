976464f47a1b2e59f7deb8176f19361da9ed60f0
ScolipedeFucker <maynouari@gmail.com>
2025-11-07 15:44:29 +0100

    created transition points for each room

--- scripts/hallway.gd
+++ scripts/hallway.gd
@@ -4,8 +4,13 @@ extends Node2D
 
 var angle: float = 0.0
 var arduino_timer : Timer
+var switch = false
+var target
 
 func _ready() -> void:
+	$ButtonHintLab.visible = false
+	$ButtonHintOutside.visible = false
+
 	scene_transition.play("fade_out")
 
 	arduino_timer = Timer.new()
@@ -25,3 +30,30 @@ func process_serial():
 	
 	$player/Arduino.update_compass(true, angle)
 
+func _process(delta: float) -> void:
+	if switch && Input.is_action_just_pressed("e"):
+		#var target = load("res://scenes/ti_lab.gd")
+		$SceneTransitionAnimation.change_scene(target)
+
+func _on_door_body_entered_tilab(body: Node2D) -> void:
+	$ButtonHintLab.visible = true
+	if body is Player:
+		global.transition_scene = true
+		switch = true
+		target = load("res://scenes/ti_lab.tscn")
+	pass # Replace with function body.
+	
+func _on_door_body_entered_outside(body: Node2D) -> void:
+	$ButtonHintOutside.visible = true
+	if body is Player:
+		global.transition_scene = true
+		switch = true
+		target = load("res://scenes/outdoor.tscn")
+	pass # Replace with function body.
+	
+func _on_door_body_exited(body: Node2D) -> void:
+	$ButtonHintLab.visible = false
+	$ButtonHintOutside.visible = false
+	if body is Player:
+		switch = false
+	pass # Replace with function body.
